ce6779a717ce07b70ddc2bb6e13ad35c20df49ba
Zeah <arild@effektivmarkedsforing.no>
2018-09-21 11:33:41 +0200

    55

---
+++ em-kredittkort-se.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: EM Kredittkort Sverige
+Description: kredittkort liste for sverige
+Version: 0.0.1
+GitHub Plugin URI: zeah/EM-kredittkort-se
+*/
+
+defined('ABSPATH') or die('Blank Space');
+
+// constant for plugin location
+define('KREDITTKORT_SE_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once 'inc/kkse-posttype.php';
+require_once 'inc/kkse-shortcode.php';
+
+function init_emkredittkortse() {
+	Kkse_posttype::get_instance();
+	Kkse_shortcode::get_instance();
+}
+add_action('plugins_loaded', 'init_emkredittkortse');
